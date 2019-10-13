960c865b0fa1d3ce7a08543c505302c5440be30c
anusha riaz <anushar058@gmail.com>
2019-10-13 00:21:43 -0700

    my first commit

---
+++ assignmment5.py
@@ -0,0 +1,16 @@
+x=6
+print (x)
+y=x+7
+print (y)
+
+print ('you must be the legendary god that comes from the sky')
+print ('we have been waiting for you a long time')
+print ('out legend says you will be very tasty with mustard')
+print ('we will have a feast tonight untill you say')
+
+X=43
+X=X+1
+print (X)
+print ('i hate you python')
+print ("good bye")
+print ("hello world")
