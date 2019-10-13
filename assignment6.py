ca72518b62ce537214f4aa3c6e590bffab2c08aa
anusha riaz <anushar058@gmail.com>
2019-10-13 00:39:02 -0700

    my first commit

---
+++ assignment6.py
@@ -0,0 +1,26 @@
+5
+x=5
+x+1
+
+name=input('enter your name:chuck\n')
+
+hours=input('enter hours:')
+rate=input('enter rate:')
+pay=float(hours)*float(rate)
+print (pay)
+pay=input('pay:')
+
+width=17
+hieght=12.0
+width//2
+type (width//2)
+width/2.0
+type (width/2.0)
+hieght/3
+type (hieght/3)
+1+2*5
+type (1+2*5)
+
+celsius=input('enter temperature in celsius:')
+farhenhiet_temperature=(float(celsius)*(9/5))+32
+print (farhenhiet_temperature)
